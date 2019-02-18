841b2e1e09cb3e206bc7bdcdc84ea2f45ef2d748
JackPound <jackpound@live.com>
2019-02-18 14:30:53 -0800

    reverted path change

--- pack_a_pack/settings.py
+++ pack_a_pack/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backpack',
+    'pack-a-pack.backpack',
 ]
 
 MIDDLEWARE = [
